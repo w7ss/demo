d519e12e3b1ceb07f82fd6eb7d89c2c34d30bab2
w7ss <1026058093@qq.com>
2020-07-11 22:19:06 +0800

    omitted

--- config/src/main/java/com/cyb/config/ConfigApplication.java
+++ config/src/main/java/com/cyb/config/ConfigApplication.java
@@ -2,10 +2,13 @@ package com.cyb.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigApplication {
 
     public static void main(String[] args) {
