5536f76bc930ff998a644f4ef9f1ae284ead7baa
w7ss <1026058093@qq.com>
2020-08-25 15:36:36 +0800

    omitted

--- my-consumer/src/main/java/com/cyb/myconsumer/MyConsumerApplication.java
+++ my-consumer/src/main/java/com/cyb/myconsumer/MyConsumerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.server.ErrorPageRegistry;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
