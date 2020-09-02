9f0fa6a78323516c3dada60767870af91bc41d84
Administrator <975873553@qq.com>
2020-09-02 17:34:33 +0800

    omitted

--- gateway/src/main/java/com/cyb/demo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cyb/demo/gateway/GatewayApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAsync
 public class GatewayApplication {
 
     public static void main(String[] args) {
@@ -20,12 +22,12 @@ public class GatewayApplication {
      * @param builder
      * @return
      */
-    @Bean
+    /*@Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("path_route", r -> r.path("/csdn")
                         .uri("https://blog.csdn.net"))
                 .build();
-    }
+    }*/
 
 }
--- my-service/src/main/java/com/cyb/myservice/controller/DemoController.java
+++ my-service/src/main/java/com/cyb/myservice/controller/DemoController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/demo")
+//@RequestMapping("/demo")
 public class DemoController {
     @GetMapping(value = "/get")
     public Person pullPerson() throws InterruptedException {
